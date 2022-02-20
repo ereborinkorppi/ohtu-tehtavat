a6f17c93a39178abd601183c4bcc7a2f28dbe035
Eero Jantunen <jantunen.eero@gmail.com>
2022-02-20 22:59:47 +0200

    tehtävä 4 oikeasti tehty

--- Viikko4/verkkokauppa/src/tests/kauppa_test.py
+++ Viikko4/verkkokauppa/src/tests/kauppa_test.py
@@ -79,3 +79,30 @@ class TestKauppa(unittest.TestCase):
         self.kauppa.tilimaksu("pekka", "12345")
 
         self.pankki_mock.tilisiirto.assert_called_with("pekka", 42, "12345", self.kauppa._kaupan_tili, 5)
+    
+    def test_aloita_asiointi_nollaa_ostoskorin(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(2)
+
+        self.assertEqual(self.kauppa._ostoskori.hinta(), 4)
+
+    def test_uusi_viitenumero_uudelle_tapahtumalle(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("pekka", "12345")
+
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.tilimaksu("mikko", "66666")
+
+        self.assertEqual(self.viitegeneraattori_mock.uusi.call_count, 2)
+        
+    def test_metodi_poista_korista_toimiii(self):
+        self.kauppa.aloita_asiointi()
+        self.kauppa.lisaa_koriin(1)
+        self.kauppa.lisaa_koriin(2)
+        self.kauppa.poista_korista(2)
+
+        self.assertEqual(self.kauppa._ostoskori.hinta(), 5)
